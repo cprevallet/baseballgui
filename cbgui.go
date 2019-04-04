8c5fc4899873e76140e39ddfd750d4e3bb2f196b
Craig Prevallet <penguintx@hotmail.com>
2019-04-04 17:31:02 -0500

    Space key to launch cannonball + gofmt.

--- cbgui.go
+++ cbgui.go
@@ -20,18 +20,17 @@ package main
 
 import (
 	"fmt"
-        "math"
-        
-        "image"
+	"math"
+
+	"image"
+	_ "image/png"
 	"os"
-        _ "image/png"
 
 	"github.com/cprevallet/baseballgui/trajectory"
-        "github.com/faiface/pixel"
-        "github.com/faiface/pixel/imdraw"
-        "github.com/faiface/pixel/pixelgl"
-        "golang.org/x/image/colornames"
-
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 func loadPicture(path string) (pixel.Picture, error) {
@@ -75,108 +74,114 @@ func doCalc(initialAltitude float64, initialAngle float64, initialVelocity float
 			history[k].Acceleration[1])
 	}
 	fmt.Println("End of Baseball")
-        return history
+	return history
 }
 
 func run() {
-        // One-time initialization section
-        cfg := pixelgl.WindowConfig{
-                Title:  "Cannonball trajectory visualization.",
-                Bounds: pixel.R(0, 0, 1024, 768),
-                VSync:  true,
-        }
-        win, err := pixelgl.NewWindow(cfg)
-        if err != nil {
-                panic(err)
-        }
-
-        imd := imdraw.New(nil)
-        inc := 0
-	
-        var Altitude float64 = 1609.0 //meters
-        var Angle float64 = 40.0  // degrees from horizontal
-        //var Velocity float64 = 35.0 // m/s
-        // this isn't historically accurate, but useful to keep it within the screen resolution
-        var Velocity float64 = 100.0 // m/s 
-        
-	trj := doCalc(Altitude, Angle, Velocity)
-        var newtrj []trajectory.TrajectoryPoint
-
-        pic, err := loadPicture("cannonball.png")
+	// One-time initialization section
+	cfg := pixelgl.WindowConfig{
+		Title:  "Cannonball trajectory visualization.",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
-        }
-        cannonball := pixel.NewSprite(pic, pic.Bounds())
-        pic2, err := loadPicture("cannon.png")
+	}
+
+	imd := imdraw.New(nil)
+	inc := 0
+
+	var Altitude float64 = 1609.0 //meters
+	var Angle float64 = 40.0      // degrees from horizontal
+	//var Velocity float64 = 35.0 // m/s
+	// this isn't historically accurate, but useful to keep it within the screen resolution
+	var Velocity float64 = 100.0 // m/s
+
+	var trj []trajectory.TrajectoryPoint
+	var newtrj []trajectory.TrajectoryPoint
+
+	pic, err := loadPicture("cannonball.png")
 	if err != nil {
 		panic(err)
-        }
-        cannon := pixel.NewSprite(pic2, pic2.Bounds())
+	}
+	cannonball := pixel.NewSprite(pic, pic.Bounds())
+	pic2, err := loadPicture("cannon.png")
+	if err != nil {
+		panic(err)
+	}
+	cannon := pixel.NewSprite(pic2, pic2.Bounds())
 
-        for !win.Closed() {
-                win.Clear(colornames.Blue)
-                imd.Clear()
+	for !win.Closed() {
 
-                // Draw a cannonball sprite
-        	mat := pixel.IM
-        	mat = mat.Scaled(pixel.ZV, 0.1 )
-        	mat = mat.Moved(pixel.V(trj[inc].Position[0], trj[inc].Position[1]))
-                cannonball.Draw(win, mat)
+		win.Clear(colornames.Blue)
+		imd.Clear()
 
-                // Draw a cannon sprite
-        	mat = pixel.IM
-        	mat = mat.Scaled(pixel.ZV, 0.2 )
-        	mat = mat.Rotated(pixel.ZV, (Angle-35.0)*math.Pi/180.0)
-                cannon.Draw(win, mat)
+		// Draw a cannonball sprite
+		mat := pixel.IM
+		if (trj != nil) && (inc < len(trj)) {
+			mat = mat.Scaled(pixel.ZV, 0.1)
+			mat = mat.Moved(pixel.V(trj[inc].Position[0], trj[inc].Position[1]))
+			cannonball.Draw(win, mat)
+		}
 
-                // Draw power graph
-                launcherX := 40.0
-                launcherY := 40.0
-                power := Velocity / 5.0
-                imd.Color = colornames.Red
-                offset := 10.0
-                imd.Push(pixel.V(launcherX + offset, launcherY + offset))
-                imd.Push(pixel.V(launcherX + offset + power, launcherY + offset))
-                imd.Line(1.0)
-                // Draw the trajectory
-                imd.Draw(win)
-                win.Update()
-                inc++
-                if inc > len(trj)-1 {
-                        inc = 0
-                        if newtrj != nil {
-                           trj = nil
-                           trj = newtrj
-                           newtrj = nil
-                        }
+		// Draw a cannon sprite
+		mat = pixel.IM
+		mat = mat.Scaled(pixel.ZV, 0.2)
+		mat = mat.Rotated(pixel.ZV, (Angle-35.0)*math.Pi/180.0)
+		cannon.Draw(win, mat)
 
-                }
-                // Accept keyboard input and update the trajectory.
-                if win.Pressed(pixelgl.KeyRight) {
+		// Draw power graph
+		launcherX := 40.0
+		launcherY := 40.0
+		power := Velocity / 5.0
+		imd.Color = colornames.Red
+		offset := 10.0
+		imd.Push(pixel.V(launcherX+offset, launcherY+offset))
+		imd.Push(pixel.V(launcherX+offset+power, launcherY+offset))
+		imd.Line(1.0)
+		// Draw the trajectory
+		imd.Draw(win)
+		win.Update()
+
+		// Update the current position of the cannonball.
+		if (trj != nil) && (inc < len(trj)) {
+			inc++
+		}
+		if (trj != nil) && (inc == len(trj)) {
+			trj = nil
+			newtrj = nil
+			inc = 0
+		}
+		if (newtrj != nil) && (trj == nil) {
+			trj = newtrj
+		}
+
+		// Accept keyboard input and calculate a new trajectory.
+
+		if win.Pressed(pixelgl.KeySpace) {
+			newtrj = doCalc(Altitude, Angle, Velocity)
+		}
+
+		if win.Pressed(pixelgl.KeyRight) {
 			Velocity += 1.0
-	                newtrj = doCalc(Altitude, Angle, Velocity)
 		}
 
-                if win.Pressed(pixelgl.KeyLeft) {
+		if win.Pressed(pixelgl.KeyLeft) {
 			Velocity -= 1.0
-	                newtrj = doCalc(Altitude, Angle, Velocity)
 		}
 
-                if win.Pressed(pixelgl.KeyUp) {
+		if win.Pressed(pixelgl.KeyUp) {
 			Angle += 1.0
-	                newtrj = doCalc(Altitude, Angle, Velocity)
 		}
 
-                if win.Pressed(pixelgl.KeyDown) {
+		if win.Pressed(pixelgl.KeyDown) {
 			Angle -= 1.0
-	                newtrj = doCalc(Altitude, Angle, Velocity)
 		}
 
-        }
+	}
 }
-
 
 func main() {
-        pixelgl.Run(run)
+	pixelgl.Run(run)
 }
-
